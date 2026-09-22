7446350e1019eb383452418f2aa5f526be1501bf
ryu-fem <ryufem181@gmail.com>
2026-09-22 20:06:52 +0300

    Add finalBotId to config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,3 +2,4 @@ export const TELEGRAM_BOT_USERNAME =
   process.env.NEXT_PUBLIC_TELEGRAM_BOT_USERNAME ?? "";
 
 export const finalUsername = TELEGRAM_BOT_USERNAME.replace(/^@/, "");
+export const finalBotId = process.env.NEXT_PUBLIC_TELEGRAM_BOT_ID || "8769306244";
